3b70cf850d542f6432e22d796e65eb128d89ea63
IvoDimov <s23521507@onlineedu.tu-varna.bg>
2023-12-04 18:41:57 +0200

    Implement fuctions for entering and showing monitors

--- MonitorStorage/Source.cpp
+++ MonitorStorage/Source.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
-const int N = 5;
+const int N = 100;
 
 struct Monitors
 {
@@ -11,16 +12,83 @@ struct Monitors
 		string color;
 		float price;
 		float size;
-		float rezolution;
-		bool tvTuner;
+		string resolution;
+		bool tvTuner=false;
 		string type;
-		string status;
+		string status = "Selling";
 };
 
-void showMainMenu()
+void showMonitors(Monitors m[],int n)
+{
+	cout << "Id" << setw(10)
+		<< "Manufacturer" << setw(10)
+		<< "Model" << setw(10)
+		<< "Color" << setw(10)
+		<< "Price" << setw(10)
+		<< "Display" << setw(10)
+		<< "Resolution" << setw(10)
+		<< "TV Tuner" << setw(10)
+		<< "Type" << setw(10)
+		<< "Status" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << m[i].id << setw(20)
+			<< m[i].manufacturer << setw(20)
+			<< m[i].model << setw(20)
+			<< m[i].color << setw(20)
+			<< m[i].price << '$' << setw(20)
+			<< m[i].size << "\"" << setw(20)
+			<< m[i].resolution << setw(20);
+		if (m[i].tvTuner == true)
+		{
+			cout << "Yes" << setw(20);
+		}
+		else
+		{
+			cout << "No" << setw(20);
+		}
+		cout << m[i].type << setw(20)
+			<< m[i].status << endl;
+	}
+}
+
+void enterMonitor(Monitors m[], int n)
+{
+	string temp,temp1="Yes";
+	int res;
+	for (int i = 1; i <= n; i++)
+	{
+		m[i].id = i;
+		cout<<endl << "Monitor's Manufacturer: ";
+		getline(cin, m[i].manufacturer);
+		cout << endl << "Monitor's Model: ";
+		getline(cin, m[i].model);
+		cout << endl << "Monitor's Color: ";
+		getline(cin, m[i].color);
+		cout << endl << "Monitor's Price: ";
+		cin >> m[i].price;
+		cout << endl << "Monitor's Display: ";
+		cin >> m[i].size;
+		cout << endl << "Monitor's Rezolution: ";
+		getline(cin, m[i].resolution);
+		cout << "Does it have a TV Tuner?: ";
+		getline(cin, temp);
+		cout << "What is it's type?(New, Second-hand): ";
+		getline(cin, m[i].type);
+
+
+		res = temp.compare(temp1);
+		if (res==0)
+		{
+			m[i].tvTuner = true;
+		}
+	}
+}
+
+void showMainMenu(Monitors m[])
 {
 	int choice;
-
+	int n;
 	do
 	{
 		cout << "1. Enter Monitor" << endl;
@@ -35,8 +103,11 @@ void showMainMenu()
 		switch (choice)
 		{
 		case 1:
+			cout << "How many monitors do you want to enter?: "; cin >> n;
+			enterMonitor(m, n);
 			break;
 		case 2:
+			showMonitors(m, n);
 			break;
 		case 3:
 			break;
@@ -58,6 +129,7 @@ void showMainMenu()
 
 int main()
 {
-	showMainMenu();
+	Monitors m[N];
+	showMainMenu(m);
 	return 0;
 }
